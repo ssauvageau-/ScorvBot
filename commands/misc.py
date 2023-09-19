2676da852c2c854112423411ad2e0458ebcd24dc
Samuel Sauvageau <smark.sauvageau@gmail.com>
2023-09-19 15:05:57 -0400

    File extensions aren't real.

--- commands/misc.py
+++ commands/misc.py
@@ -14,21 +14,23 @@ class MiscCommandCog(commands.Cog):
     @app_commands.command(name="mobile")
     async def mobile_image(self, interaction: discord.Interaction, user: discord.User):
         avatar = user.avatar
+        fn = str(user.id) + "_temp"
+        fn_o = "mobile_output"
         if avatar.is_animated():
-            fn = str(user.id) + "_temp.gif"
-            fn_o = "mobile_output.gif"
+            fn_o = f"{fn_o}.gif"
             frame_list = []
             await avatar.save(fn)
             with Image.open(fn) as im:
                 idx = 1
                 duration = 0
+
                 for frame in ImageSequence.Iterator(im):
-                    # name = f"{str(user.id)}_{idx}.png"
-                    # frame.save(name)
                     frame_list.append(self.build_mobile_frame(frame))
                     idx += 1
                     duration += im.info["duration"]
+
                 frame_duration = int(idx / duration)
+
             frame_list[0].save(
                 fn_o,
                 save_all=True,
@@ -39,34 +41,22 @@ class MiscCommandCog(commands.Cog):
             )
 
             await interaction.response.send_message(file=discord.File(fp=fn_o))
-
-            if os.path.exists(fn):
-                os.remove(fn)
-            else:
-                print(f"Error occurred when deleting file:\t{fn}")
-            if os.path.exists(fn_o):
-                os.remove(fn_o)
-            else:
-                print(f"Error occurred when deleting file:\t{fn_o}")
-
         else:
-            fn = str(user.id) + "_temp.png"
-            fn_o = "mobile_output.png"
+            fn = f"{fn}.png"
+            fn_o = f"{fn_o}.png"
             await avatar.save(fn)
-
             icon = Image.open(fn)
             self.build_mobile_frame(icon).save(fn_o)
-
             await interaction.response.send_message(file=discord.File(fp=fn_o))
 
-            if os.path.exists(fn):
-                os.remove(fn)
-            else:
-                print(f"Error occurred when deleting file:\t{fn}")
-            if os.path.exists(fn_o):
-                os.remove(fn_o)
-            else:
-                print(f"Error occurred when deleting file:\t{fn_o}")
+        if os.path.exists(fn):
+            os.remove(fn)
+        else:
+            print(f"Error occurred when deleting file:\t{fn}")
+        if os.path.exists(fn_o):
+            os.remove(fn_o)
+        else:
+            print(f"Error occurred when deleting file:\t{fn_o}")
 
     def build_mobile_frame(self, icon):
         mobile_discord = Image.open("images/MobileDiscord.png")
