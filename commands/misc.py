cce757a5a6becea8df7896e14cc7d1f3df87bf94
Samuel Sauvageau <smark.sauvageau@gmail.com>
2023-09-19 14:42:34 -0400

    Animated Mobile command.

--- commands/misc.py
+++ commands/misc.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageSequence
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -15,14 +15,21 @@ class MiscCommandCog(commands.Cog):
     async def mobile_image(self, interaction: discord.Interaction, user: discord.User):
         avatar = user.avatar
         if avatar.is_animated():
-            return
-        else:
-            fn = str(user.id) + "_temp.png"
-            fn_o = "mobile_output.png"
+            fn = str(user.id) + "_temp.gif"
+            fn_o = "mobile_output.gif"
+            frame_list = []
             await avatar.save(fn)
-
-            icon = Image.open(fn)
-            self.build_mobile_frame(icon, fn_o)
+            with Image.open(fn) as im:
+                idx = 1
+                duration = 0
+                for frame in ImageSequence.Iterator(im):
+                    # name = f"{str(user.id)}_{idx}.png"
+                    # frame.save(name)
+                    frame_list.append(self.build_mobile_frame(frame))
+                    idx += 1
+                    duration += im.info['duration']
+                frame_duration = int(idx/duration)
+            frame_list[0].save(fn_o, save_all=True, append_images=frame_list[1:], optimize=False, duration=frame_duration, loop=0)
 
             await interaction.response.send_message(
                 file=discord.File(fp=fn_o)
@@ -37,12 +44,32 @@ class MiscCommandCog(commands.Cog):
             else:
                 print(f"Error occurred when deleting file:\t{fn_o}")
 
-    def build_mobile_frame(self, icon, output_fn):
+        else:
+            fn = str(user.id) + "_temp.png"
+            fn_o = "mobile_output.png"
+            await avatar.save(fn)
+
+            icon = Image.open(fn)
+            self.build_mobile_frame(icon).save(fn_o)
+
+            await interaction.response.send_message(
+                file=discord.File(fp=fn_o)
+            )
+
+            if os.path.exists(fn):
+                os.remove(fn)
+            else:
+                print(f"Error occurred when deleting file:\t{fn}")
+            if os.path.exists(fn_o):
+                os.remove(fn_o)
+            else:
+                print(f"Error occurred when deleting file:\t{fn_o}")
+
+    def build_mobile_frame(self, icon):
         mobile_discord = Image.open("images/MobileDiscord.png")
         iresize = icon.resize((43, 43), Image.LANCZOS)
         x1, y1 = 221, 148
         x2, y2 = 264, 191
         mask = iresize.convert("RGBA")
         mobile_discord.paste(iresize, (x1, y1, x2, y2), mask)
-        mobile_discord.save(output_fn)
-        return output_fn
+        return mobile_discord
