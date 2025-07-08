dc10f0ff3b21887cca5f5e3e7c0330f6285b819d
ssauvageau- <smark.sauvageau@gmail.com>
2025-07-08 12:55:03 -0400

    New+Delete invite handling, removed some TODOs.
    Added logging.

--- src/utils/invite_tracking.py
+++ src/utils/invite_tracking.py
@@ -29,13 +29,6 @@ class InviteTracker(commands.Cog, name="invites"):
         self.redis_client = redis_client
         self.log_channel_name = "scorv-log"
 
-    def encode_invite(self, data: str) -> str:
-        return base64.b64encode(data.encode("utf-8")).decode("utf-8")
-
-    def decode_invite(self, data: str) -> str:
-        return base64.b64decode(data.encode("utf-8")).decode("utf-8")
-
-    # TODO
     async def init_invites(self):
         gld = discord.utils.find(
             lambda guild: str(guild.id) == self.guild_id, self.bot.guilds
@@ -47,6 +40,8 @@ class InviteTracker(commands.Cog, name="invites"):
             sid = str(inv.id)
             if sid in prior_load:
                 jval = json.loads(prior_load[sid])
+                if inv.uses == jval[0]:
+                    continue
                 await self.redis_client.hset(
                     name=REDIS_INVITES, key=sid, value=json.dumps(jval)
                 )
@@ -56,7 +51,6 @@ class InviteTracker(commands.Cog, name="invites"):
                 )
                 # [number of times invite was used, tracked users who used the invite]
 
-    # TODO
     @commands.Cog.listener(name="on_member_join")
     async def update_invites(self, member: discord.Member):
         gld = discord.utils.find(
@@ -76,7 +70,7 @@ class InviteTracker(commands.Cog, name="invites"):
             return
         for inv, values in pre_join_invs.items():
             # list comprehension to make code concise, should only be one item ([0])
-            hits = [x for x in post_join_invs if x.id == inv]
+            hits = [x for x in post_join_invs if str(x.id) == inv]
             jval = json.loads(values)
             if hits and hits[0].uses > jval[0]:
                 users = jval[1].append(member.id)
@@ -85,6 +79,19 @@ class InviteTracker(commands.Cog, name="invites"):
                     key=str(hits[0].id),
                     value=json.dumps([jval[0] + 1, users]),
                 )
+                self.logger.info(f"Added {member.id} to Invite Roster of Invite {inv}")
+
+    @commands.Cog.listener(name="on_invite_create")
+    async def new_invite(self, invite: discord.Invite):
+        await self.redis_client.hset(
+            name=REDIS_INVITES, key=invite.id, value=json.dumps([0, []])
+        )
+        self.logger.info(f"Added new Invite: {invite.id}")
+
+    @commands.Cog.listen(name="on_invite_deleted")
+    async def del_invite(self, invite: discord.Invite):
+        await self.redis_client.delete(str(invite.id))
+        self.logger.info(f"Deleted Invite: {invite.id}")
 
     # TODO
     @commands.Cog.listener(name="on_member_ban")
